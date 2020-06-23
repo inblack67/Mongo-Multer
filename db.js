60342b6d3c76171fa8d65ac81bc284bff4857e2c
Aman Bhardwaj <amanbhardwaj@Amans-MacBook-Air.local>
2020-06-23 23:14:55 +0530

    backend finished

--- db.js
+++ db.js
@@ -1,10 +1,6 @@
-const mongoose = require('mongoose');
-const multer = require('multer');
-const GridFSStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
-const path = require('path');
+const mongoose = require("mongoose");
 
-connectDB = async () => {
+const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             useCreateIndex: true,
@@ -12,44 +8,12 @@ connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
-
-        let gfs
-        let conn = mongoose.connection;
-
-        conn.once('open', () => {
-            gfs = Grid(conn.db, mongoose.mongo);
-            gfs.collection('uploads');
-        })
-
-
-        const storage = new GridFSStorage({
-            url: process.env.MONGO_URI,
-            options: {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            },
-            file: (req, res) => {
-                return new Promise((resolve, reject) => {
-                    crypto.randomBytes(16, (err, buf) => {
-                        if(err){
-                            return reject(err);
-                        }
-                        const fileName = buf.toString('hex') + path.extname(file.originalname)
-                        const fileInfo = {
-                            fileName,
-                            bucketName: 'uploads'
-                        };
-                        resolve(fileInfo);
-                    }) 
-                })
-            }
-        })
-
-        upload = multer({ storage });
-    
         console.log(`Mongo is here`.blue.bold); 
 
     } catch (err) {
         console.error(`${err}`.red.bold);
     }
 }
+
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const methodOverride = require('method-override');
 const multer = require('multer');
 const mongoose = require('mongoose');
-const GridFSStorage = require('multer-gridfs-storage');
+const GridFsStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 const path = require('path');
 const crypto = require('crypto');
@@ -17,24 +17,7 @@ app.use(methodOverride('_method'));
 dotenv.config({ path: './config.env' });
 
 // ==========SHIT STARTS HERE
-
-let upload;
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        console.log(`Mongo is here`.blue.bold); 
-
-    } catch (err) {
-        console.error(`${err}`.red.bold);
-    }
-}
-
+const connectDB = require('./db');
 connectDB();
 
 let conn = mongoose.connection;
@@ -42,41 +25,89 @@ let gfs;
 let mygfs;
 
 conn.once('open', () => {
+  // Init stream
   gfs = Grid(conn.db, mongoose.mongo);
   gfs.collection('uploads');
-  console.log(gfs.files);
-})
+});
 
-console.log(mygfs);
+// Create storage engine
+const storage = new GridFsStorage({
 
-const storage = new GridFSStorage({
-    url: process.env.MONGO_URI,
-    options: {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    },
-    file: (req, file) => {
-        return new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-                if(err){
-                    return reject(err);
-                }
-                const fileName = buf.toString('hex') + path.extname(file.originalname)
-                const fileInfo = {
-                    fileName,
-                    bucketName: 'uploads'
-                };
-                resolve(fileInfo);
-            }) 
-        })
-    }
-})
+  url: process.env.MONGO_URI,
 
-upload = multer({ storage });
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+
+  options: {
+    useUnifiedTopology: true
+  }
+
+});
+
+const upload = multer({ storage });
 
 app.post('/upload', upload.single('file'), (req, res) => {
     res.status(201).json({ success: true, data: req.file })
 })
+
+app.get('/upload', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: 'No files exist'
+      });
+    }
+
+    // Files exist
+    return res.status(200).json({ success: true, count: files.length, files });
+  });
+});
+
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
+
+app.delete('/files/:id', (req, res) => {
+  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+
+    res.status(200).json({ success: true, msg: 'File deleted' });
+  });
+});
 
 
 // =======SHIT ENDS HERE
